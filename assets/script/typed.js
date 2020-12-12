a93317b1545534aef1cbebdbfdb1d79879a4d35d
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-12-12 22:50:07 +0700

    first

---
+++ assets/script/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+  // Waits 1000ms after typing "First"
+  strings: ["Web Developer", "Designer", "Blogger"],
+  typeSpeed: 100,
+  delaySpeed: 150,
+  loop: true,
+});
